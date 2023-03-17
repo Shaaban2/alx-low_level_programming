551f0bb40549025e237b2156d6b6faff9ee37de8
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-03-16 17:48:22 -0700

    9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,17 @@
 #include <unistd.h>
-#define ERR_MSG "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
 /**
  * main - Entry point
  *
- * Description: program that prints a message to the standard error
+ * Description: program that prints a string to stderr
  *
- * Return: Always 1 (failure)
+ * Return: Always 1 (error)
  */
 
 int main(void)
 {
-    ssize_t len = sizeof(ERR_MSG) - 1;
-    ssize_t written = 0;
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    while (written < len)
-        written += write(STDERR_FILENO, ERR_MSG + written, len - written);
-
-    return (1);
+	write(2, str, 59);
+	return (1);
 }
-
