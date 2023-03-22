36e4c9d85d82c6ef150b17dc79ed549a62a6c9b7
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-03-22 15:28:34 -0700

    my commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Description: Finds and prints the first 98 Fibonacci numbers
  * starting with 1 and 2
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-unsigned long int i, fib1 = 1, fib2 = 2, next;
+int count = 0, num1 = 1, num2 = 2, fib = 0;
 
-printf("%lu, %lu", fib1, fib2);
+printf("%d, %d", num1, num2);
+count = 2;
 
-for (i = 2; i < 98; i++)
+while (count < 98)
 {
-next = fib1 + fib2;
-printf(", %lu", next);
-fib1 = fib2;
-fib2 = next;
+fib = num1 + num2;
+printf(", %d", fib);
+num1 = num2;
+num2 = fib;
+count++;
 }
-
 printf("\n");
+
 return (0);
 }
 
