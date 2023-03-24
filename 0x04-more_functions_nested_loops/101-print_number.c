229da2175628b5b65a8b170055a941f5e4256380
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-03-24 14:50:36 -0700

    my learning

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,19 +3,25 @@
 /**
  * print_number - prints an integer
  * @n: the integer to print
+ *
+ * Return: void
  */
 void print_number(int n)
 {
 unsigned int num;
 
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 num = -n;
-} else {
+}
+else
+{
 num = n;
 }
 
-if (num / 10) {
+if (num / 10)
+{
 print_number(num / 10);
 }
 
