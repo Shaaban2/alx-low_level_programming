9a892ed16fd81006eb2ce329d9763dec4813a806
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-03-28 19:34:06 -0700

    g

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,7 +4,6 @@
  *
  * Return: the converted integer
  */
-
 int _atoi(char *s)
 {
 int result = 0;
@@ -18,14 +17,6 @@ sign *= -1;
 else if (*s >= '0' && *s <= '9')
 {
 digit = *s - '0';
-
-if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
-{
-// result will overflow if we add this digit, so stop parsing
-// and return the appropriate value based on the sign
-return (sign == 1) ? INT_MAX : INT_MIN;
-}
-
 result = result * 10 + digit;
 }
 else if (result > 0)
