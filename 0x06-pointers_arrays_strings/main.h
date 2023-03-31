dd20021ffe73febc44f364c66cbbfaa96675ae80
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-03-30 19:50:07 -0700

    my commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
 
+/**
+ *  * This header file has all prototypes for the 0x06. C - More pointers, arrays and strings
+ *   */
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -11,4 +14,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
