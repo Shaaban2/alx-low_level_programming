ea7d614e3db714f716f1b450dd7904ef960ffe44
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-04-03 18:27:43 -0700

    learning pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
-* _strstr - function that locates a substring
-* @haystack: pointer to string
-* @needle: pointer to substring
-* Return: pointer to the beginning of the located substring or NULL if not found
-*/
-
+ * _strstr - finds the first occurrence of the substring needle in the string haystack
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the located substring, or NULL if the substring is not found
+ */
 char *_strstr(char *haystack, char *needle)
 {
 char *p1, *p2, *p3;
-
-if (!*needle)
-{
-return (haystack);
-}
 
 for (p1 = haystack; *p1 != '\0'; p1++)
 {
 p2 = p1;
 p3 = needle;
 
-while (*p3 != '\0' && *p2 == *p3)
+while (*p2 == *p3 && *p2 != '\0' && *p3 != '\0')
 {
 p2++;
 p3++;
