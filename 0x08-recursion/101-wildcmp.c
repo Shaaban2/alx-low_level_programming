59a9615d3c1d0883e255c324e4569135c7621374
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-04-06 19:48:32 -0700

    Learn

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,43 +1,33 @@
 #include "main.h"
-/*
- * A function that compares two strings and determines if they match, taking
- * into account the presence of wildcard characters ('*') in the second string.
- *
- * @param s1: a pointer to the first string to compare
- * @param s2: a pointer to the second string to compare, which may contain
- *            wildcard characters
- * @return: 1 if the strings match, 0 if they do not
- */
+
+/**
+
+wildcmp - Compares two strings.
+
+@s1: pointer to the first string to be compared
+
+@s2: pointer to the second string to be compared
+
+Return: 1 if the strings can be considered identical, 0 otherwise.
+*/
 int wildcmp(char *s1, char *s2)
 {
-    // Base case: both strings are empty
-    if (*s1 == '\0' && *s2 == '\0') {
-        return 1;
-    }
-
-    // If the first string has reached its end, check if the second string
-    // only contains wildcard characters
-    if (*s1 == '\0' && *s2 == '*') {
-        return wildcmp(s1, s2 + 1);
-    }
-
-    // If the second string has reached its end, the strings do not match
-    if (*s2 == '\0') {
-        return 0;
-    }
-
-    // If the current characters match, continue comparing the rest of the strings
-    if (*s1 == *s2) {
-        return wildcmp(s1 + 1, s2 + 1);
-    }
-
-    // If the current character in s2 is a wildcard, try to match the
-    // remaining part of s1 with and without the wildcard
-    if (*s2 == '*') {
-        return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
-    }
-
-    // If none of the above cases apply, the strings do not match
-    return 0;
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && s2 == '')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
 }
 
+if (s2 == '')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
+}
