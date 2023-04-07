8314681cec6aae8fc25dbfababa9cff80617fe98
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-04-06 19:36:48 -0700

    Learn

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,25 @@
 #include "main.h"
-/*
- * A recursive function to calculate the integer square root of a given number.
- * @param n: the input integer to calculate the square root of
- * @return: the integer square root of n, or -1 if n is negative or
- * has no integer square root
+/**
+ * _sqrt_recursion - Returns the square root of a
+ * given number using recursion.
+ * @n: The number to find the square root of.
+ *
+ * Return: The square root of n, or -1 if n does not
+ * have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
 
+/**
+ * _sqrt - Calculates the square root of a number using recursion.
+ * @n: The number to find the square root of.
+ * @i: The current value to test for a square root.
+ *
+ * Return: The square root of n, or -1 if n does not have a
+ * natural square root.
+ */
 int _sqrt(int n, int i)
 {
 	if (n < 0)
