e7ed489597d7f786ae1caa677d4e6b6af140ca8e
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-04-06 19:34:12 -0700

    Learn

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /*
- * Recursive function to calculate the power of a given base raised to an exponent.
+ * Recursive function to calculate the power of a
+ * given base raised to an exponent.
  * @param base: the base integer
  * @param exponent: the exponent integer
  * @return: the result of base raised to the power of exponent
