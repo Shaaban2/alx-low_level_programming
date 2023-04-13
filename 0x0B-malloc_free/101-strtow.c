5064bc65002ce1665a2a3e6a29185602eb636d33
Shaaban2 <eng-shaaban.reda1520@alexu.edu.eg>
2023-04-12 20:09:24 -0700

    d

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,13 +9,13 @@
  */
 void ch_free_grid(char **grid, unsigned int height)
 {
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
+if (grid != NULL && height != 0)
+{
+for (; height > 0; height--)
+free(grid[height]);
+free(grid[height]);
+free(grid);
+}
 }
 
 /**
@@ -34,44 +34,44 @@ void ch_free_grid(char **grid, unsigned int height)
  */
 char **strtow(char *str)
 {
-	char **aout;
-	unsigned int c, height, m, k, a1;
+char **aout;
+unsigned int c, height, m, k, a1;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
-	{
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	}
-	aout = malloc((height + 1) * sizeof(char *));
-	if (aout == NULL || height == 0)
-	{
-		free(aout);
-		return (NULL);
-	}
-	for (m = a1 = 0; m < height; m++)
-	{
-		for (c = a1; str[c] != '\0'; c++)
-		{
-			if (str[c] == ' ')
-				a1++;
-			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			{
-				aout[m] = malloc((c - a1 + 2) * sizeof(char));
-				if (aout[m] == NULL)
-				{
-					ch_free_grid(aout, m);
-					return (NULL);
-				}
-				break;
-			}
-		}
-		for (k = 0; a1 <= c; a1++, k++)
-			aout[m][k] = str[a1];
-		aout[m][k] = '\0';
-	}
-	aout[m] = NULL;
-	return (aout);
+if (str == NULL || *str == '\0')
+return (NULL);
+for (c = height = 0; str[c] != '\0'; c++)
+{
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+height++;
+}
+aout = malloc((height + 1) * sizeof(char *));
+if (aout == NULL || height == 0)
+{
+free(aout);
+return (NULL);
+}
+for (m = a1 = 0; m < height; m++)
+{
+for (c = a1; str[c] != '\0'; c++)
+{
+if (str[c] == ' ')
+a1++;
+if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+{
+aout[m] = malloc((c - a1 + 2) * sizeof(char));
+if (aout[m] == NULL)
+{
+ch_free_grid(aout, m);
+return (NULL);
+}
+break;
+}
+}
+for (k = 0; a1 <= c; a1++, k++)
+aout[m][k] = str[a1];
+aout[m][k] = '\0';
+}
+aout[m] = NULL;
+return (aout);
 }
 
